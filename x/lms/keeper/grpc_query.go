c814aaf081cf4cb0ece88a5a518b0361edd84dab
saiteja <tejargukt123@gmail.com>
2023-02-23 16:42:10 +0530

    implemented query checkstatus

--- x/lms/keeper/grpc_query.go
+++ x/lms/keeper/grpc_query.go
@@ -3,13 +3,17 @@ package keeper
 import (
 	"context"
 	"lmsmodule/x/lms/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ types.QueryServer = Keeper{}
 
-func (k Keeper) ListLeaves(context.Context, *types.ListLeavesRequest) (*types.ListLeavesResponse, error) {
+func (k Keeper) ListLeaves(ctx context.Context, listLeavesRequest *types.ListLeavesRequest) (*types.ListLeavesResponse, error) {
 	return &types.ListLeavesResponse{}, nil
 }
-func (k Keeper) LeaveStatus(context.Context, *types.LeaveStatusRequest) (*types.LeaveStatusResponse, error) {
-	return &types.LeaveStatusResponse{}, nil
+func (k Keeper) LeaveStatus(ctx context.Context, leaveStatusRequest *types.LeaveStatusRequest) (*types.LeaveStatusResponse, error) {
+	sdkctx := sdk.UnwrapSDKContext(ctx)
+	leave, _ := k.CheckLeaveStatus(sdkctx, leaveStatusRequest.Address)
+	return &types.LeaveStatusResponse{Leave: &leave}, nil
 }
--- x/lms/keeper/student.go
+++ x/lms/keeper/student.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"lmsmodule/x/lms/types"
 	"log"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -28,10 +29,37 @@ func (k Keeper) AddStudent(ctx sdk.Context, student *types.Student) error {
 	return nil
 }
 
-func (k Keeper) GetStudent(ctx sdk.Context, req string) []byte {
-	if _, err := sdk.AccAddressFromBech32(req); err != nil {
+func (k Keeper) GetStudent(ctx sdk.Context, studentaddress string) []byte {
+	if _, err := sdk.AccAddressFromBech32(studentaddress); err != nil {
 		log.Fatal(err)
 	}
 	store := ctx.KVStore(k.storeKey)
-	return store.Get(types.StudentStoreKey(req))
+	return store.Get(types.StudentStoreKey(studentaddress))
+}
+
+func (k Keeper) CheckLeaveStatus(ctx sdk.Context, studentAddress string) (types.Leave, error) {
+	if _, err := sdk.AccAddressFromBech32(studentAddress); err != nil {
+		return types.Leave{}, err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	if store.Get(types.StudentStoreKey(studentAddress)) == nil {
+		return types.Leave{}, types.ErrStudentDoesNotExist
+	}
+
+	leaveId, _ := strconv.Atoi(string(store.Get(types.LeaveCounterStoreKey(studentAddress))))
+
+	// if a student never applied for a leave
+	if leaveId == 0 {
+		return types.Leave{}, types.ErrLeaveNeverApplied
+	}
+
+	val := store.Get(types.LeaveStatusStoreKey(studentAddress, leaveId))
+	var leave *types.MsgAcceptLeaveRequest
+	k.cdc.Unmarshal(val, leave)
+	return types.Leave{
+		Address: leave.Admin,
+		Status:  (leave.Status == types.LeaveStatus_STATUS_ACCEPTED),
+	}, nil
+
 }
